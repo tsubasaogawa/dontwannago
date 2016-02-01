3ccce4ae419d6b892b090f272535740c0624b2b8
Tsubasa Ogawa <togawa@media.eng.hokudai.ac.jp>
2016-02-01 23:41:54 +0900

    fix dbs select (latest 5 items only)

--- app/controllers/twis_controller.rb
+++ app/controllers/twis_controller.rb
@@ -5,6 +5,7 @@
 class TwisController < ApplicationController
   def index
     @log = Logs.all
-	@chart_data = Logs.all.order('date').group(:date).sum(:count)
+	# @chart_data = Logs.all.order('date').group(:date).sum(:count)
+	@chart_data = Logs.select(:date, :count).limit(5).order(date: :desc).pluck(:date, :count)
   end
 end
