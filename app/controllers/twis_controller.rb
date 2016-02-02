3769eeb8a6d23666e37402dadee1e571d71cb4aa
Tsubasa Ogawa <togawa@media.eng.hokudai.ac.jp>
2016-02-02 22:34:21 +0900

    fix typo exit to end

--- app/controllers/twis_controller.rb
+++ app/controllers/twis_controller.rb
@@ -7,7 +7,7 @@ class TwisController < ApplicationController
     @log = Logs.all
     # @chart_data = Logs.all.order('date').group(:date).sum(:count)
     days = params[:days].to_i
-    end if days <= 0 or days > 365
+    exit if days <= 0 or days > 365
     @chart_data = Logs.select(:date, :count).limit(days).order(date: :desc).pluck(:date, :count)
   end
 end
